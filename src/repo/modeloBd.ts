c92531f80d12f7d6ca86219343e75b77f279b82b
augustofav <augustofavetti159@gmail.com>
2024-10-31 22:30:04 -0300

    start logic

---
+++ src/repo/modeloBd.ts
@@ -0,0 +1,16 @@
+// models/Event.ts
+export interface Modelmateria {
+    id: number;
+    nome: string;
+    data: Date;
+    
+  }
+  
+  // models/Participante.ts
+  export interface serviceProfessor {
+    id: number;
+    nome: string;
+    email: string;
+   
+  }
+  
